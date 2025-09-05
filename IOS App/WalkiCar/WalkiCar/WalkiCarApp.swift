7368de55e218f15c2d303100565e2b1d23071eaf
blu2772 <tim@chipworker.de>
2025-09-05 23:35:24 +0200

    First

---
+++ IOS App/WalkiCar/WalkiCar/WalkiCarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkiCarApp.swift
+//  WalkiCar
+//
+//  Created by Tim Rempel on 05.09.25.
+//
+
+import SwiftUI
+
+@main
+struct WalkiCarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS App/WalkiCar/WalkiCarTests/WalkiCarTests.swift
@@ -0,0 +1,16 @@
+//
+//  WalkiCarTests.swift
+//  WalkiCarTests
+//
+//  Created by Tim Rempel on 05.09.25.
+//
+
+import Testing
+
+struct WalkiCarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
